dbb531cced9767cf0ad5184f54ff4f193cf7b51b
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-08-02 21:28:51 +0700

    navbar complete

---
+++ script.js
@@ -0,0 +1,29 @@
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+// Navbar toogl
+
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+};
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+};
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
